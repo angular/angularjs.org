ad555f303623a8aad55267e2d62111668f82dd2a
Vojta Jina <vojta@gemin-i.org>
2011-04-28 13:24:44 +0200

    testimonials - limit opinion length
    Change testimonials a bit - instead of rendering all opinions and changing visibitily,
    we only change angular expressions (content, name).
    And limit the content size if necesarry.
    
    This whole logic (limiting text and displaying link to unfold full text) would be nice
    filter or widget. (could be filter, but filter should not change DOM...

--- js/app.js
+++ js/app.js
@@ -4,9 +4,10 @@
  * @returns {TestimonialsCtrl}
  */
 function TestimonialsCtrl() {
-  this.selected = 0;
+  // init - first opinion
+  this.next(0);
 
-  // change testimonial every 20 secs
+  // change opinion every 20 secs
   var self = this;
   setInterval(function() {
     self.next();
@@ -19,8 +20,40 @@ TestimonialsCtrl.prototype = {
   /**
    * Display the next testimonial
    */
-  next: function() {
-    this.selected = (this.selected + 1) % this.TESTIMONIALS.length;
+  next: function(index) {
+    this.selected_ = arguments.length ? index : this.getNextIndex_();
+    this.loadOpinion_(this.TESTIMONIALS[this.selected_]);
+  },
+  
+  /**
+   * Load given opinion and truncate the content if necesarry
+   * @param {Object} opinion
+   */
+  loadOpinion_: function(opinion) {
+    if (opinion.content.length > 380) {
+      this.content = opinion.content.substr(0, 356);
+      this.isOpen = false;
+    } else {
+      this.content = opinion.content;
+      this.isOpen = true;
+    }
+    this.name = opinion.name;
+  },
+
+  /**
+   * Count index of next opinion (testimonial)
+   * @returns {Number}
+   */
+  getNextIndex_: function() {
+    return (this.selected_ + 1) % this.TESTIMONIALS.length;
+  },
+
+  /**
+   * Unfold current opinion
+   */
+  open: function() {
+    this.isOpen = true;
+    this.content = this.TESTIMONIALS[this.selected_].content;
   },
 
   /**
@@ -29,7 +62,7 @@ TestimonialsCtrl.prototype = {
   TESTIMONIALS: [{
     name: 'John Hardy',
     content: 'Also I want to pass on my compliments to Misko and Igor for this fantastic project. I\'m currently rewriting a server-side web application to use this system. I am constantly astounded at how much simpler it is to do it this way and I still consider myself a learner.' +
-      'This is without question the most productive approach to building webapps that I have seen.' +
+      'This is without question the most productive approach to building webapps that I have seen. ' +
       'The last time I had a coding epiphany was discovering the power and simplicity of JQuery. This is way better than that.' +
       'I\'m interested in promoting this library as widely as possible. I understand that you are still developing it and I still have a long way to go before I really understand everything but I think you really have something here.'
   }, {
@@ -38,7 +71,7 @@ TestimonialsCtrl.prototype = {
   }, {
     name: 'Dobrica Pavlinusic',
     content: 'Thanks to great help I received at this list, I was basically able to accomplish my goal to write simple conference submission application within a week of first git clone of angular source from github.' +
-      'I think it might be useful to summarize my experience here, especially for people who are still wondering if angular is worth a try. Executive summary is: yes it is!'
+      'I think it might be useful to summarize my experience here, especially for people who are still wondering if angular is worth a try. Executive summary is: yes it is !'
   }]
 };
 
