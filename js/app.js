65526bcb2a3ca31f449b7db428a0fbced3c233fb
Misko Hevery <misko@hevery.com>
2012-03-08 16:45:05 -0800

    ugraded to 1.0.0_rc1

--- js/app.js
+++ js/app.js
@@ -1,20 +1,16 @@
 /**
  * Application controller for the entire page
  */
-function AppCtrl($location) {
-  var scope = this;
-
-  scope.angular = {version: '0.9.19', name: 'canine-psychokinesis'};
+function AppCtrl($scope, $location) {
+  $scope.angular = {version: '1.0.0_rc1', name: 'moiré-vision'};
 
   //normalize hash, set subpage and track page view
-  scope.$watch(function() { return $location.hash; }, function() {
-    var hash = $location.hash;
-
+  $scope.$watch(function() { return $location.path(); }, function(hash) {
     if (hash == '/' || hash == '/downloads' || hash == '/community') {
-      scope.subpage = hash.substring(1);
+      $scope.subpage = hash.substring(1);
       _gaq.push(['_trackPageview', hash]);
     } else {
-      $location.hash = '/';
+      $location.path('/');
     }
   });
 }
@@ -25,134 +21,111 @@ function AppCtrl($location) {
  *
  * @returns {TestimonialsCtrl}
  */
-function TestimonialsCtrl() {
-  this.loadOpinion_(0);
-}
-
-TestimonialsCtrl.prototype = {
-
+function TestimonialsCtrl($scope, $defer) {
+  var timer_;
   /**
-   * Display the previous testimonial
+   * Array of testimonials
    */
-  previous: function() {
-    if (this.selected_ == 0)
-      this.loadOpinion_(this.TESTIMONIALS.length - 1);
+  $scope.TESTIMONIALS = [
+    { name: 'Ray Camden', // http://www.coldfusionjedi.com/
+      content: 'I am simply blown away. ... the tutorials themselves are doing a great job ' +
+        'introducing new concepts and - frankly - making me excited about using the ' +
+        'platform. I haven\'t been this excited since I got into jQuery.' },
+    { name: 'John Hardy',
+      content: 'I\'m currently rewriting a server-side web application to use this system. I am constantly astounded at how much simpler it is to do it this way and I still consider myself a learner.' +
+        'This is without question the most productive approach to building webapps that I have seen. ' +
+        'The last time I had a coding epiphany was discovering the power and simplicity of JQuery. This is way better than that.' +
+        'I\'m interested in promoting this library as widely as possible. I understand that you are still developing it and I still have a long way to go before I really understand everything but I think you really have something here.' }, 
+    { name: 'Jerry Jeremiah',
+      content: 'Angular is the best thing I have used in a long time. I am having so much fun, even thought it is probably obvious that dynamic web sites are new to me (my experience is more in the back end embedded world...)' }, 
+    { name: 'Dobrica Pavlinusic',
+      content: 'Thanks to great help I received at this list, I was basically able to accomplish my goal to write simple conference submission application within a week of first git clone of angular source from github.' +
+        'I think it might be useful to summarize my experience here, especially for people who are still wondering if angular is worth a try. Executive summary is: yes it is !' },
+    { name: 'Tobias Bosch',
+      content: 'Angular is great work, thank you! I found it while I was writing an own client side templating system using custom html tags, but didn\'t know how to figure out a good way to implement data binding. I definitely think that this is a great way to go.'},
+    { name: 'Johan Steenkamp',
+      content: 'For me, Angular these days is to web development, what jQuery was to JavaScript.' }
+  ];
+
+  $scope.previous = function() {
+    if ($scope.selected_ == 0)
+      loadOpinion($scope.TESTIMONIALS.length - 1);
     else
-      this.loadOpinion_(this.selected_ - 1);
-  },
+      loadOpinion($scope.selected_ - 1);
+  };
 
   /**
    * Display the next testimonial
    */
-  next: function() {
-    this.loadOpinion_((this.selected_ + 1) % this.TESTIMONIALS.length);
-  },
+  $scope.next = function() {
+    loadOpinion(($scope.selected_ + 1) % $scope.TESTIMONIALS.length);
+  };
 
   /**
    * Load opinion with given index and truncate the content if necessary
    * @param {Number} index
    */
-  loadOpinion_: function(index) {
-    this.selected_ = index;
-    var opinion = this.TESTIMONIALS[index];
+  function loadOpinion(index) {
+    $scope.selected_ = index;
+    var opinion = $scope.TESTIMONIALS[index];
     if (opinion.content.length > 380) {
-      this.content = opinion.content.substr(0, 356);
-      this.isOpen = false;
+      $scope.content = opinion.content.substr(0, 356);
+      $scope.isOpen = false;
     } else {
-      this.content = opinion.content;
-      this.isOpen = true;
+      $scope.content = opinion.content;
+      $scope.isOpen = true;
     }
-    this.name = opinion.name;
-    this.resetTimer_();
-  },
+    $scope.name = opinion.name;
+    resetTimer();
+  }
 
   /**
    * Reset timer = clear old timeout and set new one
    */
-  resetTimer_: function() {
-    if (this.timer_) {
-      clearTimeout(this.timer_);
+  function resetTimer() {
+    if (timer_) {
+      $defer.cancel(timer_);
     }
 
-    var self = this;
-    this.timer_ = setTimeout(function() {
-      self.next();
-      self.$eval();
-    }, 20000);
-  },
+    timer_ = $defer($scope.next, 20000);
+  }
 
   /**
    * Unfold current opinion
    */
-  open: function() {
-    this.isOpen = true;
-    this.content = this.TESTIMONIALS[this.selected_].content;
-  },
+  $scope.open = function() {
+    $scope.isOpen = true;
+    $scope.content = $scope.TESTIMONIALS[$scope.selected_].content;
+  };
 
-  /**
-   * Array of testimonials
-   */
-  TESTIMONIALS: [{
-    name: 'Ray Camden', // http://www.coldfusionjedi.com/
-    content: 'I am simply blown away. ... the tutorials themselves are doing a great job ' +
-             'introducing new concepts and - frankly - making me excited about using the ' +
-             'platform. I haven\'t been this excited since I got into jQuery.'
-  }, {
-    name: 'John Hardy',
-    content: 'I\'m currently rewriting a server-side web application to use this system. I am constantly astounded at how much simpler it is to do it this way and I still consider myself a learner.' +
-      'This is without question the most productive approach to building webapps that I have seen. ' +
-      'The last time I had a coding epiphany was discovering the power and simplicity of JQuery. This is way better than that.'
-  }, {
-    name: 'Jerry Jeremiah',
-    content: 'Angular is the best thing I have used in a long time. I am having so much fun, even thought it is probably obvious that dynamic web sites are new to me (my experience is more in the back end embedded world...)'
-  }, {
-    name: 'Dobrica Pavlinusic',
-    content: 'Thanks to great help I received at this list, I was basically able to accomplish my goal to write simple conference submission application within a week of first git clone of angular source from github.' +
-      'I think it might be useful to summarize my experience here, especially for people who are still wondering if angular is worth a try. Executive summary is: yes it is !'
-  }, {
-    name: 'Tobias Bosch',
-    content: 'Angular is great work, thank you! I found it while I was writing an own client side templating system using custom html tags, but didn\'t know how to figure out a good way to implement data binding. I definitely think that this is a great way to go.'
-  }, {
-    name: 'Johan Steenkamp',
-    content: 'For me, Angular these days is to web development, what jQuery was to JavaScript.'
-  }]
-};
+  loadOpinion(0);
+}
 
 /**
  * Live Examples Controller
  *
  * @returns {ExamplesCtrl}
  */
-function ExamplesCtrl() {
+function ExamplesCtrl($scope) {
   SyntaxHighlighter.highlight();
-  this.selected = 0;
+
+
+  $scope.selected = 0;
+
+  $scope.examples = [
+    {title: 'Password', description: 'The following is a demo password generator app that showcases angular\'s rich declarative templates, data-binding, MVC, xhr service, and depenency injection.'},
+    {title: 'Invoice', description: 'The following is a demo invoicing app built only with angular\'s declarative templates, data-binding, filters, and validators.'}
+  ];
+
+
+  $scope.previous = function() {
+    $scope.selected = Math.max(0, $scope.selected - 1);
+  }
+
+  $scope.next = function() {
+    $scope.selected = Math.min($scope.examples.length - 1, $scope.selected + 1);
+  }
 }
-
-ExamplesCtrl.prototype = {
-  previous: function() {
-    if (this.selected == 0)
-      this.selected = this.examples.length - 1;
-    else
-      this.selected = this.selected - 1;
-  },
-  next: function() {
-    this.selected = (this.selected + 1) % this.examples.length;
-  },
-
-  examples: [{
-               title: 'Todo List',
-               fiddle: 'IgorMinar/M8YnR/',
-               description: 'The following is a todo app that allows you to add tasks and remove tasks that are done. This app showcases angular\'s rich declarative templates, data-binding, MVC.'
-             }, {
-               title: 'Password',
-               fiddle: 'vojtajina/76E4S/',
-               description: 'This is a password generator app that allows you to enter a password or obtain a password from server via xhr service, and determine its strength on the fly. This app showcases angular\'s rich declarative templates, data-binding, MVC, xhr service, and depenency injection.'
-             }, {
-               title: 'Invoice',
-               fiddle: 'vojtajina/pfqKY/',
-               description:'The following is a invoicing app written purely in angular\'s declarative templates that allows you to add or remove items, fill in quantity, description and cost, and compute total costs on the fly. This app showcases angular\'s declarative templates, data-binding, filters, and validators.'
-            }]
-};
 
 // syntax highlighter defaults
 SyntaxHighlighter.defaults['html-script'] = true;
--- js/doc_widgets.js
+++ js/doc_widgets.js
@@ -1,94 +1,76 @@
-(function(){
+angular.module('homepage', []).
+  directive('docExample', function() {
 
-  var angularJsUrl;
-  var scripts = document.getElementsByTagName("script");
-  var angularJsRegex = /^(|.*\/)angular(-.*?)?(\.min)?.js(\?[^#]*)?(#(.*))?$/;
-  for(var j = 0; j < scripts.length; j++) {
-    var src = scripts[j].src;
-    if (src && src.match(angularJsRegex)) {
-      angularJsUrl = src.replace('docs.angularjs.org', 'code.angularjs.org');
-      continue;
-    }
-  }
-
-
-  var HTML_TEMPLATE =
-  '<!doctype html>\n' +
-  '<html xmlns:ng="http://angularjs.org">\n' +
-  ' <script src="' + angularJsUrl + '"\n' +
-  '   ng:autobind></script>\n' +
-  ' <body>\n' +
-  '_HTML_SOURCE_\n' +
-  ' </body>\n' +
-  '</html>';
-
-  angular.widget('doc:example', function(element){
-    this.descend(true); //compile the example code
-    element.hide();
-
-    var example = element.find('pre.doc-source').eq(0),
-        exampleSrc = example.text(),
-        scenario = element.find('pre.doc-scenario').eq(0);
-
-    var code = indent(exampleSrc);
-    
-    // Vojta:
-    // - removed highlighting
-    // - moved params (no template, html-script) into defaults...
-    // - swapped source with preview
-    var tabHtml =
-      '<ul class="doc-example">' +
-        '<li class="doc-example-heading"><h3>Live Preview</h3></li>' +
-        '<li class="doc-example-live">' + exampleSrc +'</li>' +
-        '<li class="doc-example-heading"><h3>Source</h3></li>' +
-        '<li class="doc-example-source" ng:non-bindable>' +
-          '<pre class="brush: js"></pre></li>';
-
-    if (scenario.text()) {
-      tabHtml +=
-        '<li class="doc-example-heading"><h3>Scenario Test</h3></li>' +
-        '<li class="doc-example-scenario"><pre class="brush: js">' + scenario.text() + '</pre></li>';
-    }
-    tabHtml +=
-      '</ul>';
-    var tabs = angular.element(tabHtml);
-
-    tabs.find('li.doc-example-source > pre').text(HTML_TEMPLATE.replace('_HTML_SOURCE_', code.html));
-
-    element.html('');
-    element.append(tabs);
-    element.show();
-
-    var script = (exampleSrc.match(/<script[^\>]*>([\s\S]*)<\/script>/) || [])[1] || '';
-    try {
-      eval(script);
-    } catch (e) {
-      alert(e);
-    }
-  });
-
-  function indent(text) {
-    if (!text) return text;
-    var lines = text.split(/\r?\n|\r/);
-    var lineNo = [];
-    // remove any leading blank lines
-    while (lines[0].match(/^\s*$/)) lines.shift();
-    // remove any trailing blank lines
-    while (lines[lines.length - 1].match(/^\s*$/)) lines.pop();
-    var minIndent = 999;
-    for ( var i = 0; i < lines.length; i++) {
-      var line = lines[0];
-      var indent = line.match(/^\s*/)[0];
-      if (indent !== line && indent.length < minIndent) {
-        minIndent = indent.length;
+    var angularJsUrl;
+    var scripts = document.getElementsByTagName("script");
+    var angularJsRegex = /^(|.*\/)angular(-.*?)?(\.min)?.js(\?[^#]*)?(#(.*))?$/;
+    for(var j = 0; j < scripts.length; j++) {
+      var src = scripts[j].src;
+      if (src && src.match(angularJsRegex)) {
+        angularJsUrl = src.replace('docs.angularjs.org', 'code.angularjs.org');
+        continue;
       }
     }
 
-    for ( var i = 0; i < lines.length; i++) {
-      lines[i] = '  ' + lines[i].substring(minIndent);
-      lineNo.push(5 + i);
-    }
-    return {html: lines.join('\n'), hilite: lineNo.join(',') };
-  };
 
-})();
+    var HTML_TEMPLATE =
+      '<!doctype html>\n' +
+        '<html ng-app>\n' +
+        ' <script src="' + angularJsUrl + '"></script>\n' +
+        ' <body>\n' +
+        '_HTML_SOURCE_\n' +
+        ' </body>\n' +
+        '</html>';
+
+
+    return {
+      restrict: 'C',
+      priority: 1000,
+      compile: function(element) {
+
+        var exampleSrc = indent(element.text());
+        var ul = angular.element('<ul class="doc-example">');
+        var example = angular.element('<li class="doc-example-live">').html(exampleSrc)
+
+        element.replaceWith(ul);
+        ul.append('<li class="doc-example-heading"><h3>Live Preview</h3></li>');
+        ul.append(example);
+        ul.append('<li class="doc-example-heading"><h3>Suorce</h3></li>');
+        ul.append(
+          angular.element('<li class="doc-example-source" ng-non-bindable>').append(
+            angular.element('<pre  class="brush: js">').text(HTML_TEMPLATE.replace('_HTML_SOURCE_', exampleSrc))));
+
+        var script = (exampleSrc.match(/<script[^\>]*>([\s\S]*)<\/script>/) || [])[1] || '';
+        try {
+          window.eval(script);
+        } catch (e) {
+          alert(e);
+        }
+      }
+    };
+
+    ///////////////////////////////
+    ///////////////////////////////
+
+    function indent(text) {
+      if (!text) return text;
+      var lines = text.split(/\r?\n|\r/);
+      // remove any leading blank lines
+      while (lines[0].match(/^\s*$/)) lines.shift();
+      // remove any trailing blank lines
+      while (lines[lines.length - 1].match(/^\s*$/)) lines.pop();
+      var minIndent = 999;
+      for ( var i = 0; i < lines.length; i++) {
+        var line = lines[0];
+        var indent = line.match(/^\s*/)[0];
+        if (indent !== line && indent.length < minIndent) {
+          minIndent = indent.length;
+        }
+      }
+
+      for ( var i = 0; i < lines.length; i++) {
+        lines[i] = '  ' + lines[i].substring(minIndent);
+      }
+      return lines.join('\n');
+    };
+  });
