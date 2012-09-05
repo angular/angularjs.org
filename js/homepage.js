8286cfc3a040f02e9291c1296781e8a593276818
Igor Minar <igor@angularjs.org>
2012-09-05 10:48:59 -0700

    upgrade to 1.0.2 + Google CDN

--- js/homepage.js
+++ js/homepage.js
@@ -82,8 +82,8 @@ angular.module('homepage', [])
   .factory('script', function() {
 
     return {
-      angular: '<script src="http://code.angularjs.org/angular-' + angular.version.full + '.min.js"></script>\n',
-      resource: '<script src="http://code.angularjs.org/angular-resource-' + angular.version.full + '.min.js"></script>\n'
+      angular: '<script src="//ajax.googleapis.com/ajax/libs/angularjs/' + angular.version.full + '/angular.min.js"></script>\n',
+      resource: '<script src="//ajax.googleapis.com/ajax/libs/angularjs/' + angular.version.full + '/angular-resource.min.js"></script>\n'
     };
   })
 
