e6e7982c140721489d7cb94294090fcd5dac0ba1
Jeff Cross <middlefloor@gmail.com>
2013-11-25 15:38:37 -0800

    feat(propagate): update propagate script to use http and targetpool API

--- gitFetchSite.js
+++ gitFetchSite.js
@@ -1,39 +1,75 @@
-var exec = require('child_process').exec;
+var exec = require('child_process').exec,
+    http = require('http'),
+    TARGET_POOL = 'ng-sites',
+    REGION = 'us-central1',
+    PROJECT = '435162472401',
+    PORT = '8000';
 
 console.log('Beginning propagation to other instances');
 
-exec('gcutil listinstances --format=json --project=435162472401', function (err, result, code) {
+exec('gcutil gettargetpool '+ TARGET_POOL +' --project='+ PROJECT +' --region='+ REGION +' --format=json', function (err, result, code) {
   var instanceIPs = [],
-      executed = 0;
+      executed = 0,
+      instances;
 
   if (err) {
     throw new Error(err);
   }
 
-  var regions = JSON.parse(result).items;
+  try {
+    targetPool = JSON.parse(result);
+    instances = targetPool.instances;
+  }
+  catch (e) {
+    console.error('Could not parse target pool');
+    return process.exit(1);
+  }
 
-  Object.keys(regions).forEach(function (key) {
-    var instances = regions[key].instances;
-    if (Array.isArray(instances)) {
-      instances.forEach(function (instance) {
-        instance.networkInterfaces && instance.networkInterfaces.forEach(function (netInt) {
-          netInt.accessConfigs &&
-            netInt.accessConfigs[0] &&
-            parseInt(netInt.accessConfigs[0].natIP, 10) &&
-            instanceIPs.push(netInt.accessConfigs[0].natIP);
+
+  instances.forEach(function (instance) {
+    var name = /^.*\/([a-zA-Z\-0-9]*)$/.exec(instance)[1];
+
+    exec('gcutil getinstance '+ name +' --format=json --project='+ PROJECT, function (err, result, code) {
+      var instance, reqUrl, exitCode = 0;
+
+      if (err) {
+        console.error(err);
+        return process.exit(code);
+      }
+
+      try {
+        instance = JSON.parse(result);
+        console.log('instance', instance);
+        instance.networkInterfaces.forEach(function (netInt) {
+          if (reqUrl) return;
+
+          console.log('netInt', netInt);
+
+          netInt.accessConfigs.forEach(function (config) {
+            console.log('config', config, config.natIP);
+            if (config.natIP) reqUrl = 'http://'+ config.natIP +':'+ PORT +'/gitFetchSite.php?doNotPropagate=true';
+            console.log('reqUrl', reqUrl);
+          });
         });
+      }
+      catch (e) {
+        console.error(e);
+        return process.exit(1);
+      }
+
+      console.log('Updating remote instance: ', reqUrl);
+
+      http.get(reqUrl, function (res) {
+        console.log('Finished executing', reqUrl);
+        executed++;
+        executed === instanceIPs.length && process.exit(exitCode);
+      }).on('error', function (err) {
+        console.error('Failed to update', reqUrl);
+        console.error(err);
+        executed++;
+        exitCode = 1;
+        executed === instanceIPs.length && process.exit(exitCode);
       });
-    }
-  });
-
-  instanceIPs.forEach(function (ip) {
-    var reqUrl = 'http://' + ip + ':8000/gitFetchSite.php?doNotPropagate=true';
-    console.log('Updating remote instance: ', reqUrl);
-
-    exec('curl ' + reqUrl, function (err, result, code) {
-      console.log('Finished executing', reqUrl);
-      executed++;
-      executed === instanceIPs.length && process.exit(code);
     });
   });
 });
