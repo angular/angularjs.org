d4c0fb05c8f496621c3ad03226e6510c2a67a187
Peter Bacon Darwin <pete@bacondarwin.com>
2014-03-16 08:21:43 +0000

    fix(download-data): branches should not be coloured at all

--- js/download-data.js
+++ js/download-data.js
@@ -4,12 +4,12 @@ angular.module('download-data', [])
     {
       branch: '1.2.*', version: '1.2.14',
       title: '1.2.x (legacy)',
-      cssClass: 'bluePill'
+      cssClass: ''
     },
     {
       branch: '1.3.*', version: '1.3.0-beta.2',
       title: '1.3.x (latest)',
-      cssClass: 'bluePill'
+      cssClass: ''
     }
 ])
 
