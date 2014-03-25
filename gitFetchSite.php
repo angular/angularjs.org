5e92033317f7f6c34fd7cd4f5acea64a216cbb37
Jeff Cross <middlefloor@gmail.com>
2014-03-25 14:41:09 -0700

    change gitFetchSite to checkout dist branch
    The new release process will rely on building the angularjs.org
    home page and making the build artifacts available in the dist
    branch of the repository.

--- gitFetchSite.php
+++ gitFetchSite.php
@@ -7,7 +7,7 @@
 <?php `date >> gitFetchSite.log`; ?>
   <pre><?php echo `date`; ?></pre>
   <pre><?php echo `git fetch origin`; ?></pre>
-  <pre><?php echo `git checkout origin/master -f`; ?></pre>
+  <pre><?php echo `git checkout origin/dist -f`; ?></pre>
   <pre><?php echo `git status`; ?></pre>
   <pre><?php echo `git log -n1`; ?></pre>
 
