bb2bd8cc7f86f439734bda839b1f058f92d7c46c
Tobias Bosch <tbosch1009@gmail.com>
2014-06-07 07:15:44 -0700

    update site from src

--- js/download-data.js
+++ js/download-data.js
@@ -2,12 +2,12 @@ angular.module('download-data', [])
 
 .value('BRANCHES', [
     {
-      branch: '1.2.*', version: '1.2.16',
+      branch: '1.2.*', version: '1.2.17',
       title: '1.2.x (legacy)',
       cssClass: 'branch-1-2-x'
     },
     {
-      branch: '1.3.*', version: '1.3.0-beta.10',
+      branch: '1.3.*', version: '1.3.0-beta.11',
       title: '1.3.x (latest)',
       cssClass: 'branch-1-3-x'
     }
