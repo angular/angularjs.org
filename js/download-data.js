9a64e9fcd8acaa1a8364da9827c8cea09658b751
jenkins <ci-bot@angularjs.org>
2014-10-08 16:59:45 -0700

    update site from src

--- js/download-data.js
+++ js/download-data.js
@@ -7,7 +7,7 @@ angular.module('download-data', [])
       cssClass: 'branch-1-2-x'
     },
     {
-      branch: '1.3.*', version: '1.3.0-rc.4',
+      branch: '1.3.*', version: '1.3.0-rc.5',
       title: '1.3.x (latest)',
       cssClass: 'branch-1-3-x'
     }
