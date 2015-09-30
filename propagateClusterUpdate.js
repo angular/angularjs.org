cd2b21a64e744b3205b4f9a3ce974fa9e6c459d0
jenkins <ci-bot@angularjs.org>
2015-09-30 13:54:50 -0700

    update site from src

--- propagateClusterUpdate.js
+++ propagateClusterUpdate.js
@@ -7,7 +7,7 @@ var exec = require('child_process').exec,
 
 console.log('Beginning propagation to other instances');
 
-exec('gcutil gettargetpool '+ TARGET_POOL +' --project='+ PROJECT +' --region='+ REGION +' --format=json', function (err, result, code) {
+exec('dig backends.angularjs.org +short TXT', function (err, result, code) {
   var executed = 0,
       instances;
 
@@ -16,48 +16,18 @@ exec('gcutil gettargetpool '+ TARGET_POOL +' --project='+ PROJECT +' --region='+
   }
 
   try {
-    targetPool = JSON.parse(result);
-    instances = targetPool.instances;
-  }
-  catch (e) {
-    console.log('Could not parse target pool');
-    return process.exit(1);
-  }
+    var instances = result
+      .replace(/"/g,'')
+      .replace(/\n/g, '')
+      .split(',');
 
-
-  instances.forEach(function (instance) {
-    var name = /^.*\/([a-zA-Z\-0-9]*)$/.exec(instance)[1];
-
-    exec('gcutil getinstance '+ name +' --format=json --project='+ PROJECT, function (err, result, code) {
-      var instance, reqUrl, exitCode = 0;
-
-      if (err) {
-        console.log(err);
-        return process.exit(code);
-      }
-
-      try {
-        instance = JSON.parse(result);
-        instance.networkInterfaces.forEach(function (netInt) {
-          if (reqUrl) return;
-          if (netInt.networkIP) reqUrl = 'http://'+ netInt.networkIP +':'+ PORT +'/gitFetchSite.php?doNotPropagate=true';
-        });
-      }
-      catch (e) {
-        console.log(e);
-        return process.exit(1);
-      }
-
-      if (!reqUrl) {
-        return console.log('Could not find any URL for instance', instance);
-      }
-
-      console.log('Updating remote instance: ', reqUrl);
-
+    instances.forEach(function(instance) {
+      var reqUrl = 'http://'+ instance +':'+ PORT +'/gitFetchSite.php?doNotPropagate=true';
+      console.log('Updating ', reqUrl);
       http.get(reqUrl, function (res) {
         console.log('Finished executing', reqUrl);
         executed++;
-        executed === instances.length && process.exit(exitCode);
+        executed === instances.length && process.exit();
       }).on('error', function (err) {
         console.log('Failed to update', reqUrl);
         console.log(err);
@@ -65,6 +35,10 @@ exec('gcutil gettargetpool '+ TARGET_POOL +' --project='+ PROJECT +' --region='+
         exitCode = 1;
         executed === instances.length && process.exit(exitCode);
       });
-    });
-  });
+      });
+  }
+  catch (e) {
+    console.log('failed to propagate with', e);
+    return process.exit(1);
+  }
 });
