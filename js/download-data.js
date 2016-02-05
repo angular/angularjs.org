12ad1b44e3a265741bdb914bd8ee9bbdf669d121
jenkins <ci-bot@angularjs.org>
2016-02-05 09:05:53 -0800

    update site from src

--- js/download-data.js
+++ js/download-data.js
@@ -2,7 +2,7 @@ angular.module('download-data', [])
 
 .value('BRANCHES', [
     {
-      branch: '1.5.*', version: '1.5.0-rc.2',
+      branch: '1.5.*', version: '1.5.0',
       title: '1.5.x (release candidate)',
       cssClass: 'branch-1-5-x',
       showOnButton: true
