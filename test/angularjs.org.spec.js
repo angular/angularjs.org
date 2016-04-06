angular/angularjs.org

commit 1206c9971d2d66083fbc0896014ac22b01f5d371

chore: update angular-bootstrap-ui to 1.1.2
- use cdn for loading angular-bootstrap instead of hosting a copy ourselves
- add uib prefix to popover, modal and tab directives
- migrate custom unsafe html popover directive to uib-popover-html + $sce.trustAsHtml
- updating fixes broken right click in dropdowns in FF, see angular/angular.js#14024

src/js/homepage.js
@@ -216,10 +216,10 @@ angular.module('homepage', ['ngAnimate', 'ui.bootstrap', 'download-data'])
     };
   })
 
-  .directive('appSource', function(fetchCode, escape, $compile, $timeout, templateBuilder) {
+  .directive('appSource', function(fetchCode, escape, $compile, $timeout, templateBuilder, $sce) {
     return {
       terminal: true,
-      scope: true,
+      scope: {},
       link: function(scope, element, attrs) {
         var tabs = [],
             annotation = attrs.annotate && angular.fromJson(fetchCode(attrs.annotate)) || {};
@@ -251,19 +251,22 @@ angular.module('homepage', ['ngAnimate', 'ui.bootstrap', 'download-data'])
               counter = 0;
 
           angular.forEach(annotation[filename], function(text, key) {
+            counter++;
 
             text = text.replace('{{', '&#123;&#123;').replace('}}', '&#125;&#125;');
 
             var regexp = new RegExp('(\\W|^)(' + key.replace(/([\W\-])/g, '\\$1') + ')(\\W|$)');
 
+            scope['popover' + index + counter] = $sce.trustAsHtml(text);
+
             content = content.replace(regexp, function(_, before, token, after) {
-              token = "__" + (counter++) + "__";
+              token = "__" + (counter) + "__";
               popovers[token] =
                 '<span class="nocode"\n' +
                 '      popover-title="' + escape(key) + '"\n' +
-                '      popover-trigger="click"\n' +
+                '      popover-trigger="outsideClick"\n' +
                 '      popover-append-to-body="true"\n' +
-                '      popover-html-unsafe="' + escape(text) + '"><code>' + escape(key) + '</code>' +
+                '      uib-popover-html="popover' + index + counter + '"><code>' + escape(key) + '</code>' +
                 '</span>';
               return before + token + after;
             });
@@ -274,22 +277,21 @@ angular.module('homepage', ['ngAnimate', 'ui.bootstrap', 'download-data'])
           });
 
           tabs.push(
-            '<tab heading="' + (index ? filename : 'index.html')  + '">\n' +
+            '<uib-tab heading="' + (index ? filename : 'index.html')  + '">\n' +
             '  <pre class="prettyprint linenums nocode"><code>' + content +'</code></pre>\n' +
-            '</tab>\n'
+            '</uib-tab>\n'
           );
         });
 
         element.html(
-          '<tabset>' +
+          '<uib-tabset>' +
             tabs.join('') +
-          '</tabset>');
-        // element.find('[rel=popover]').popover().pulse();
+          '</uib-tabset>');
 
         // Compile up the HTML to get the directives to kick-in
         $compile(element.children())(scope);
         $timeout(function() {
-          var annotationElements = element.find('span[popover-html-unsafe]');
+          var annotationElements = element.find('span[uib-popover-html]');
           $compile(annotationElements)(scope);
         }, 0);
       }
@@ -368,23 +370,23 @@ angular.module('homepage', ['ngAnimate', 'ui.bootstrap', 'download-data'])
   .directive('hint', function() {
     return {
       template: '<em>Hint:</em> Click ' +
-          '<code class="nocode" popover-title="Hover" popover-trigger="click" popover-append-to-body="true"' +
-          'popover="Click highlighted areas in the code for explanations.">me</code>.'
+          '<code class="nocode" popover-title="Hover" popover-trigger="outsideClick" popover-append-to-body="true"' +
+          'uib-popover="Click highlighted areas in the code for explanations.">me</code>.'
     };
   })
 
-  .controller('AppController', function($scope, $modal, BRANCHES) {
+  .controller('AppController', function($scope, $uibModal, BRANCHES) {
     $scope.BRANCHES = BRANCHES;
 
     $scope.showDownloadModal = function() {
-      $modal.open({
+      $uibModal.open({
         templateUrl: 'partials/download-modal.html',
         windowClass: 'download-modal'
       });
     };
 
     $scope.showVideo = function(videoUrl) {
-      $modal.open({
+      $uibModal.open({
         templateUrl: 'partials/video-modal.html',
         windowClass: 'video-modal',
         controller: 'VideoController',
@@ -457,36 +459,6 @@ angular.module('homepage', ['ngAnimate', 'ui.bootstrap', 'download-data'])
   }, 2500);
 
 })
-
-
-// Angular UI Bootstrap provide some excellent directives, but the popover didn't allow for HTML content
-// The popoverHtmlUnsafe and popoverHtmlUnsafePopup implement this on top of the AngularUI Bootstrap's $tooltip service
-.directive( 'popoverHtmlUnsafePopup', function ($templateCache) {
-
-  $templateCache.put("template/popover/popover-html-unsafe-popup.html",
-    "<div class=\"popover {{placement}}\" ng-class=\"{ in: isOpen(), fade: animation() }\">\n" +
-    "  <div class=\"arrow\"></div>\n" +
-    "\n" +
-    "  <div class=\"popover-inner\">\n" +
-    "      <h3 class=\"popover-title\" ng-bind=\"title\" ng-show=\"title\"></h3>\n" +
-    "      <div class=\"popover-content\" bind-html-unsafe=\"content\"></div>\n" +
-    "  </div>\n" +
-    "</div>\n" +
-    "");
-
-  return {
-    restrict: 'EA',
-    replace: true,
-    scope: { title: '@', content: '@', placement: '@', animation: '&', isOpen: '&' },
-    templateUrl: 'template/popover/popover-html-unsafe-popup.html'
-  };
-})
-
-.directive( 'popoverHtmlUnsafe', [ '$compile', '$timeout', '$parse', '$window', '$tooltip', function ( $compile, $timeout, $parse, $window, $tooltip ) {
-  return $tooltip( 'popoverHtmlUnsafe', 'popover', 'click' );
-}])
-
-
 
 .run(function($rootScope, startPulse){
   $rootScope.version = angular.version;

src/index.html
@@ -47,11 +47,11 @@
             <li class="divider-vertical"></li>
             <li class="active"><a href="http://angularjs.org/"><i class="icon-home icon-white"></i> Home</a></li>
             <li class="divider-vertical"></li>
-            <li class="dropdown">
-              <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+            <li class="dropdown" uib-dropdown>
+              <a href="#" class="dropdown-toggle"  uib-dropdown-toggle>
                 <i class="icon-eye-open icon-white"></i> Learn <b class="caret"></b>
               </a>
-              <ul class="dropdown-menu">
+              <ul class="dropdown-menu" uib-dropdown-menu>
                 <li class="disabled"><a href="">Home</a></li>
                 <li><a href="https://www.youtube.com/user/angularjs">Videos</a></li>
                 <li><a href="http://angular.codeschool.com/">Free Course</a></li>
@@ -62,11 +62,11 @@
               </ul>
             </li>
             <li class="divider-vertical"></li>
-            <li class="dropdown">
-              <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+            <li class="dropdown" uib-dropdown>
+              <a href="#" class="dropdown-toggle" uib-dropdown-toggle>
                 <i class="icon-book icon-white"></i> Develop <b class="caret"></b>
               </a>
-              <ul class="dropdown-menu">
+              <ul class="dropdown-menu" uib-dropdown-menu>
                 <li><a href="http://docs.angularjs.org/tutorial">Tutorial</a></li>
                 <li><a href="http://docs.angularjs.org/guide">Developer Guide</a></li>
                 <li><a href="http://docs.angularjs.org/api">API Reference</a></li>
@@ -75,11 +75,11 @@
               </ul>
             </li>
             <li class="divider-vertical"></li>
-            <li class="dropdown">
-              <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+            <li class="dropdown" uib-dropdown>
+              <a href="#" class="dropdown-toggle" uib-dropdown-toggle>
                 <i class="icon-comment icon-white"></i> Discuss <b class="caret"></b>
               </a>
-              <ul class="dropdown-menu">
+              <ul class="dropdown-menu" uib-dropdown-menu>
                 <li><a href="http://blog.angularjs.org">Blog</a></li>
                 <li><a href="http://groups.google.com/group/angular">Mailing List</a></li>
                 <li><a href="http://webchat.freenode.net/?channels=angularjs&uio=d4">Chat Room</a></li>
@@ -990,7 +990,7 @@
   <script src="google-code-prettify/prettify.min.js"></script>
   <script src="js/homepage.js"></script>
   <script src="js/download-data.js"></script>
-  <script src="js/angular-ui-bootstrap.js"></script>
+  <script src="//cdnjs.cloudflare.com/ajax/libs/angular-ui-bootstrap/1.1.2/ui-bootstrap-tpls.min.js"></script>
   <script src="//ajax.googleapis.com/ajax/libs/angularjs/${CDN_VERSION_1_5}/angular-resource.min.js"></script>
   <script src="//ajax.googleapis.com/ajax/libs/angularjs/${CDN_VERSION_1_5}/angular-route.min.js"></script>
   <script src="https://cdn.firebase.com/js/client/2.0.4/firebase.js"></script>

commit c35d87f9701e55e883aec9574fb6a8fd6e959a35

fix: restore the explanatory popovers in the dl modal
Accidentally broken in 1206c9971d2d66083fbc0896014ac22b01f5d371

Fixes #195

src/js/homepage.js
@@ -398,7 +398,7 @@ angular.module('homepage', ['ngAnimate', 'ui.bootstrap', 'download-data'])
 
   })
 
-  .controller('DownloadController', function($scope, BRANCHES, BUILDS, DOWNLOAD_INFO) {
+  .controller('DownloadController', function($scope, $sce, BRANCHES, BUILDS, DOWNLOAD_INFO) {
 
     function getRelativeUrl(branch, build) {
       switch (build.name) {
@@ -413,7 +413,12 @@ angular.module('homepage', ['ngAnimate', 'ui.bootstrap', 'download-data'])
 
     $scope.BRANCHES = BRANCHES;
     $scope.BUILDS = BUILDS;
-    $scope.DOWNLOAD_INFO = DOWNLOAD_INFO;
+    $scope.DOWNLOAD_INFO_TRUSTED = {
+      branchesInfo: $sce.trustAsHtml(DOWNLOAD_INFO.branchesInfo),
+      buildsInfo: $sce.trustAsHtml(DOWNLOAD_INFO.buildsInfo),
+      bowerInfo: $sce.trustAsHtml(DOWNLOAD_INFO.bowerInfo),
+      cdnInfo: $sce.trustAsHtml(DOWNLOAD_INFO.cdnInfo)
+    };
 
     $scope.currentBranch = $scope.BRANCHES[0];
     $scope.currentBuild = $scope.BUILDS[0];

src/partials/download-modal.html
@@ -23,9 +23,9 @@
         </span>
 
         <a href=""
+            uib-popover-html="DOWNLOAD_INFO_TRUSTED.branchesInfo"
             popover-title="Branches"
-            popover-html-unsafe="{{DOWNLOAD_INFO.branchesInfo}}"
-            popover-trigger="click" popover-placement="bottom"
+            popover-trigger="outsideClick" popover-placement="bottom"
             popover-delay="300" popover-append-to-body="true">
               &nbsp;<i class="icon-question-sign"></i>
         </a>
@@ -42,9 +42,9 @@
           </button>
         </span>
         <a href=""
+            uib-popover-html="DOWNLOAD_INFO_TRUSTED.buildsInfo"
             popover-title="Builds"
-            popover-html-unsafe="{{ DOWNLOAD_INFO.buildsInfo }}"
-            popover-trigger="click" popover-placement="bottom"
+            popover-trigger="outsideClick" popover-placement="bottom"
             popover-delay="300" popover-append-to-body="true">
               &nbsp;<i class="icon-question-sign"></i>
         </a>
@@ -57,9 +57,9 @@
                   value="{{ cdnUrl() || 'Unavailable for zip archives' }}">
         </span>
         <a href=""
+            uib-popover-html="DOWNLOAD_INFO_TRUSTED.cdnInfo"
             popover-title="Why Google CDN?"
-            popover-html-unsafe="{{ DOWNLOAD_INFO.cdnInfo }}"
-            popover-trigger="click" popover-placement="bottom"
+            popover-trigger="outsideClick" popover-placement="bottom"
             popover-delay="300" popover-append-to-body="true">
               &nbsp;<i class="icon-question-sign"></i>
         </a>
@@ -72,9 +72,9 @@
                   value="bower install angular#{{currentBranch.version}}">
         </span>
         <a href=""
+            uib-popover-html="DOWNLOAD_INFO_TRUSTED.bowerInfo"
             popover-title="What is Bower?"
-            popover-html-unsafe="{{ DOWNLOAD_INFO.bowerInfo }}"
-            popover-trigger="click" popover-placement="bottom"
+            popover-trigger="outsideClick" popover-placement="bottom"
             popover-delay="300" popover-append-to-body="true">
               &nbsp;<i class="icon-question-sign"></i>
         </a>

test/angularjs.org.spec.js
@@ -33,7 +33,7 @@ describe('Angularjs.org', function () {
         downloadBtn.click();
         browser.driver.sleep(500);
         cdnInput = browser.findElement(protractor.By.css('#cdnURL'));
-        cdnInput.getAttribute('value')
+        cdnInput.getAttribute('value');
         cdnInput.getText().then(function (text) {
           stableVersion = text.toString().split('/').splice(-2,1)[0];
         });
@@ -80,12 +80,11 @@ describe('Angularjs.org', function () {
       });
 
 
-
       it('should allow downloading uncompressed angular', function () {
         var uncompressedBtn = browser.findElement(
             protractor.By.css(
                 '.download-modal .modal-body > dl button.uncompressed'));
-        uncompressedBtn.click()
+        uncompressedBtn.click();
 
         expect(cdnInput.getAttribute('value')).toContain('angular.js');
       });
@@ -96,6 +95,21 @@ describe('Angularjs.org', function () {
         closeButton.click();
         expect(element(by.css('.download-modal')).isPresent()).toBe(false);
       });
+
+      describe('explanatory popover', function() {
+        ['Branches', 'Builds', 'Why Google CDN?','What is Bower?'].forEach(function(value) {
+
+          it('should be displayed for ' + value, function () {
+            var popoverButton = browser.findElement(protractor.By.css('.download-modal [popover-title="'+value+'"]'));
+            popoverButton.click();
+
+            var popover = browser.findElement(protractor.By.css('.popover'));
+            expect(popover.getCssValue('display')).toEqual('block');
+          });
+
+        });
+      });
+
 
     });
 